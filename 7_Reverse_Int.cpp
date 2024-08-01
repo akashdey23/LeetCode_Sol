2da05637c351835c9fbca67d515ce836d23dc94b
Akash Dey <akashdey9128@gmail.com>
2024-08-01 17:02:01 +0530

    Update 7_Reverse_Int.cpp

--- 7_Reverse_Int.cpp
+++ 7_Reverse_Int.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        int r = 0; //declaring r here 
+        long r = 0; //declaring r here 
         while(x){
             int lastDigit = x % 10; // extracting the last digit
             // count = count + 1;
@@ -11,6 +11,6 @@ public:
             x = x/10; //next integer finding and updating the value of x
         }
         if(r>INT_MAX || r<INT_MIN) return 0; //Here returns 0 if the int region goes beyond the range given
-    return r; // return r;
+    return int(r); // return r;
     }
 };
